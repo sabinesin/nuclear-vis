8043333c115c609d07897b93ed5cfb3a492e958e
Tanner Smith <tanner@tssoftware.net>
2013-10-30 09:10:54 -0400

    Draw a blank SVG with conventional margins.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  console.log("Everything is ready!");
+  var margin = {
+    top: 10,
+    right: 10,
+    bottom: 10,
+    left: 10
+  };
+
+  var width = 960 - margin.left - margin.right;
+  var height = 500 - margin.top - margin.bottom;
+
+  var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 });
